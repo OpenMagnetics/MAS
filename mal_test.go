29a66ce187bbcb41e9ee059e115beec91bf3f4e5
AlfVII <Alfonso_VII@hotmail.com>
2022-08-06 14:34:41 +0200

    Added schema for bobbin and database of standard shapes

--- mal_test.go
+++ mal_test.go
@@ -7,9 +7,38 @@ import (
     "testing"
     "github.com/xeipuuv/gojsonschema"
     "path/filepath"
+    "io"
+    "encoding/json"
 )
 
-// TestSampleShape checks the shape sample json
+func CheckJson(t *testing.T, schema string, jsonToCheck interface{}) {
+    var documentLoader gojsonschema.JSONLoader
+
+    switch v := jsonToCheck.(type) {
+        case string:
+            documentLoader = gojsonschema.NewReferenceLoader(v)
+        case map[string]interface{}:
+            documentLoader = gojsonschema.NewGoLoader(v)
+    }
+
+    schemaLoader := gojsonschema.NewReferenceLoader(schema)
+
+    result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+    if err != nil {
+        t.Errorf(err.Error())
+    }
+
+    if result.Valid() {
+        fmt.Printf("The document is valid\n")
+    } else {
+        t.Errorf("The document is not valid. see errors above\n")
+        for _, desc := range result.Errors() {
+            fmt.Printf("- %s\n", desc)
+        }
+    }
+}
+
+// TestSampleShape checks sample jsons
 func TestSampleShape(t *testing.T) {
     module_path, _ := os.Getwd()
 
@@ -21,22 +50,7 @@ func TestSampleShape(t *testing.T) {
             relativePath := strings.Split(strings.Split(path, searchDir)[1], info.Name())[0]
             relativePath = strings.TrimRight(relativePath, "/")
 
-            schemaLoader := gojsonschema.NewReferenceLoader("file://" + module_path + "/schemas/" + relativePath + ".json")
-            documentLoader := gojsonschema.NewReferenceLoader("file://" + path)
-
-            result, err := gojsonschema.Validate(schemaLoader, documentLoader)
-            if err != nil {
-                t.Errorf(err.Error())
-            }
-
-            if result.Valid() {
-                fmt.Printf("The document is valid\n")
-            } else {
-                t.Errorf("The document is not valid. see errors above\n")
-                for _, desc := range result.Errors() {
-                    fmt.Printf("- %s\n", desc)
-                }
-            }
+            CheckJson(t, "file://" + module_path + "/schemas/" + relativePath + ".json", "file://" + path)
 
         }
         return err
@@ -44,5 +58,79 @@ func TestSampleShape(t *testing.T) {
     
     if e != nil {
         panic(e)
+    }
+}
+
+// TestShapesData checks the shapes data
+func TestShapesData(t *testing.T) {
+    module_path, _ := os.Getwd()
+
+    dataFile := module_path + "/data/shapes.ndjson"
+
+    // open the file pointer
+    shapesFile, err := os.Open(dataFile)
+    if err != nil {
+        panic(err)
+    }
+    defer shapesFile.Close()
+
+    // create a new decoder
+    var shapesDecoder *json.Decoder = json.NewDecoder(shapesFile)
+    if err != nil {
+        panic(err)
+    }
+
+    for {
+        var doc map[string]interface{}
+
+        err := shapesDecoder.Decode(&doc)
+        if err == io.EOF {
+            // all done
+            break
+        }
+        if err != nil {
+            panic(err)
+        }
+
+        fmt.Printf("name: %+v\n", doc["name"])
+        CheckJson(t, "file://" + module_path + "/schemas/core/shape.json", doc)
+
+    }
+}
+
+// TestBobbinsData checks the bobbins data
+func TestBobbinsData(t *testing.T) {
+    module_path, _ := os.Getwd()
+
+    dataFile := module_path + "/data/bobbins.ndjson"
+
+    // open the file pointer
+    bobbinsFile, err := os.Open(dataFile)
+    if err != nil {
+        panic(err)
+    }
+    defer bobbinsFile.Close()
+
+    // create a new decoder
+    var bobbinsDecoder *json.Decoder = json.NewDecoder(bobbinsFile)
+    if err != nil {
+        panic(err)
+    }
+
+    for {
+        var doc map[string]interface{}
+
+        err := bobbinsDecoder.Decode(&doc)
+        if err == io.EOF {
+            // all done
+            break
+        }
+        if err != nil {
+            panic(err)
+        }
+
+        fmt.Printf("name: %+v\n", doc["name"])
+        CheckJson(t, "file://" + module_path + "/schemas/core/bobbin.json", doc)
+
     }
 }
